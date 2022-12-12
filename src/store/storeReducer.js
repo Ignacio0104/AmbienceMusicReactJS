9ec7d35226c24f2f9f80b2844062a686bb99cecc
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-12-12 15:26:57 -0300

    comienzos

---
+++ src/store/storeReducer.js
@@ -0,0 +1,70 @@
+const types = {
+    authLogin: 'auth - login',
+    authLogout: 'auth - logout',
+    productDeleteAll: 'product - delete all',
+    productChange: 'product - change'
+}
+
+const initialStore = [{
+    id:1,
+    name:"Cozy Jazz Music at Snow Christmas Night", 
+    url:"https://www.youtube.com/watch?v=qFbY87Gm7dI",
+    theme: ["jazz","christmas","winter"],
+    picture: "https://i.ytimg.com/vi/qFbY87Gm7dI/maxresdefault.jpg?v=6395b513",
+    description: "On cold snow nights, what could be better than sitting somewhere in the middle of the noisy city, sipping your favorite drink and watching the space darken in a corner of this cozy room, on a christmas night. The relaxing and smooth melodies of Piano Jazz are enough for those who sit here to feel like they can drop their souls into that wonderful space. A truly meaningful relaxation time before the end of a long day. Take time for yourself, enjoy the peace of life through delicious cups of coffee and some soft jazz music."
+},{
+    id:2,
+    name:"Hogwarts Great Hall | Harry Potter Music & Ambience", 
+    url:"https://www.youtube.com/watch?v=_v4cgUVBsi4",
+    theme: ["movies","christmas","harry","potter"],
+    picture: "https://i.ytimg.com/vi/_v4cgUVBsi4/maxresdefault.jpg",
+    description: "Immerse yourself in the legendary Great Hall of Hogwarts School of Witchcraft and Wizardry! Think about all of the memories you would make here as a student. The sorting ceremony, holiday meals, speeches from the headmaster, and more! Relax and enjoy this 3 hour video."
+},{
+    id:3,
+    name:"Lord of the Rings | The Shire - Music & Ambience", 
+    url:"https://www.youtube.com/watch?v=30b7_S0paCQ&t=2043s",
+    theme: ["movies","relax","shire","LOTR","rings","Lord of the rings"],
+    picture: "https://i.ytimg.com/vi/30b7_S0paCQ/maxresdefault.jpg",
+    description: "I want to see the wild country again before I die, and the Mountains; but he is still in love with the Shire, with woods and fields and little rivers."
+},{
+    id:4,
+    name:"Lord of the Rings | The Shire - Music & Ambience", 
+    url:"https://www.youtube.com/watch?v=30b7_S0paCQ&t=2043s",
+    theme: ["movies","relax","shire","LOTR","rings","Lord of the rings"],
+    picture: "https://i.ytimg.com/vi/30b7_S0paCQ/maxresdefault.jpg",
+    description: "I want to see the wild country again before I die, and the Mountains; but he is still in love with the Shire, with woods and fields and little rivers."
+}]
+
+const storeReducer = (state, action) => {
+    switch(action.type) {
+        case types.authLogout:
+            return {
+                ...state,
+                user: null
+            }
+
+        case types.authLogin:
+            return {
+                ...state,
+                user: action.payload
+            }
+
+        case types.productDeleteAll:
+            return {
+                ...state,
+                products: []
+            }
+
+        case types.productChange:
+            return {
+                ...state,
+                products: [{ id: 3, title: 'Product #3' }]
+            }
+
+        default:
+            return state;
+    }
+}
+
+export { initialStore, types }
+export default storeReducer
