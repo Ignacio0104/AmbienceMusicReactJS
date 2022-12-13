5768cfe20d7d49659bee02d393cd77f72d7e6a7d
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-12-13 10:08:46 -0300

    videos section responsive

---
+++ src/components/Video.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Video.css"
+
+function Video(props) {
+  return (
+        <div className="videoPage">
+            <div className="video-container">
+            <div className="video-item">
+                <div className="video-info">
+                    <h1 className="video-title"> {props.video[0].name} </h1>
+                    <iframe className="video-image" src={props.video[0].url} title="Youtube video"></iframe>
+                    <p>{props.video[0].description}</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Video
--- src/components/pages/Videos.js
+++ src/components/pages/Videos.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import "../../App.css"
 import { useStore } from "../../store/StoreProvider";
+import Video from "../Video";
 
 
 export default function Videos(){
@@ -11,17 +12,6 @@ export default function Videos(){
     let video = videos.filter((video)=> video.id === id)
 
     return (
-        <div className="videoPage">
-            <div className="video-container">
-                <div className="video-item">
-                    <div className="video-info">
-                        <h1 className="video-title"> {video[0].name} </h1>
-                        <iframe className="video-image" src={video[0].url} title="Youtube video"></iframe>
-                        <p>{video[0].description}</p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
+      <Video video={video}></Video>
     )
 }
