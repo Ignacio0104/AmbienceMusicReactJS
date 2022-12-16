19e03d497c2fa51953731823084eec9b85d47178
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-12-16 18:31:09 -0300

    validation in form impproved

--- src/components/FormAddVideo.js
+++ src/components/FormAddVideo.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch } from '../store/StoreProvider';
 import "./FormAddVideo.css"
 
@@ -17,13 +17,13 @@ function FormAddVideo() {
     const [labelsError, setLabelsError] = useState(false);
     const [pictureError, setPictureError] = useState(false);
     const [descriptionError, setDescriptionError] = useState(false);
+    const [allowSubmit, setAllowSubmit] = useState(false)
     const labelText = useRef();
     const nameText = useRef();
     const urlText = useRef();
     const pictureText = useRef();
     const descriptionText = useRef();
 
-    
   const addVideo = ()=>{
     dispatch(
       {
@@ -40,60 +40,84 @@ function FormAddVideo() {
   }
 
     const addLabel = ()=>{
-        setLabels([...labels, labelText.current.value])
+        if(labelText.current.value.length >0)
+        {
+            setLabels([...labels, labelText.current.value]);
+            labelText.current.value = ""
+        }
     }
 
     const deleteLabel = (labelSelected)=>{
         setLabels(labels.filter((label)=>label!==labelSelected));
     }
     const validateName = ()=>{
+        enableButton();
         if(nameText.current.value.length > 5)
             setNameError(false)
         else 
             setNameError(true)
     }
     const validateUrl = ()=>{
+        enableButton();
         setUrlError(!urlText.current.value.includes("www.youtube.com/embed"))
     }
    
     const validateLabel = ()=>{
+        enableButton();
         setLabelsError(labels.length<1);
     }
     const validatePicture = ()=>{
+        enableButton();
         setPictureError(!pictureText.current.value.includes("https://") && 
         (!pictureText.current.value.includes("jpg")||!pictureText.current.value.includes("jpeg")
         ||!pictureText.current.value.includes("bmp")));
     }
 
     const validateDescription = ()=>{
-        setDescriptionError(descriptionText.current.value<30)
+        enableButton();
+        setDescriptionError(descriptionText.current.value.length<30)
     }
 
-    const validateFields = async (e)=>{
+    const enableButton = ()=>{
+        if(nameText.current.value.length >0 && urlText.current.value.length>0
+            && pictureText.current.value.length >0 && descriptionText.current.value.length >0)
+            {
+               setAllowSubmit(true);
+            }else{
+                setAllowSubmit(true);
+            }
+    }
+    
+    const updateList = ()=>{
+        
+        if(!nameError&&!urlError&&!labelsError&&!pictureError&&!descriptionError)
+        {
+            //addVideo();   
+            alert("Estoy aca")
+            /*try{  
+                alert("Estoy agregando")
+                await addDoc(collection(db,'videos'),{
+                    name:nameText.current.value, 
+                    url:urlText.current.value,
+                    theme: labels,
+                    picture: pictureText.current.value,
+                    description: descriptionText.current.value,
+                    views:0
+                })
+            }catch(err){
+                console.log(err)
+            }*/
+        }
+    }
+
+    const validateFields = (e)=>{
         e.preventDefault();
         validateName();
         validateUrl();
         validateLabel();
         validatePicture();
         validateDescription();
-        try{   //ADD A BASE DE DATOS
-            await addDoc(collection(db,'videos'),{
-                id:5,
-                name:"Beach Cafe Ambience: tropical music, ocean waves, & no worries!", 
-                url:"https://www.youtube.com/embed/oFRFaI0Ntvk",
-                theme: ["tropical","beach","ocean","cafe"],
-                picture: "https://i.ytimg.com/vi/oFRFaI0Ntvk/maxresdefault.jpg",
-                description: "Enjoy the relaxing atmosphere of this tropical beach cafe ambience with instrumental beach music, softly crashing waves, birds, and soft chatter in the background. Grab your favorite book and a cup of coffee and take a seat near the water to watch the waves gently rolling on shore. The playlist I created for this cafe ambience is a mix of relaxing Hawaiian, acoustic guitar, ukulele, island, and reggae music. Hope you enjoy your beach day vacation!",
-                views:0
-            })
-        }catch(err){
-            console.log(err)
-        }
-        if(!nameError&&!urlError&&!labelsError&&!pictureError&&!descriptionError)
-        {
-            addVideo();
-     
-        }
+        updateList();
     }
     
   return (
@@ -102,9 +126,9 @@ function FormAddVideo() {
         <div className='form-add-container'>
             <form className='form-add'>
                 <input type="text" onBlur={validateName} className={nameError && "error-border"} ref={nameText} placeholder="name"/>
-                {nameError && <p className='error-label-left-top'>Please verify the name<br/> (no less than 5 characters)</p>}
                 <input type="text" onBlur={validateUrl} className={urlError && "error-border"} ref={urlText} placeholder="url"></input>
-                {urlError && <p className='error-label-right-top'>Please verify the URL.<br/> Use a validate embed youtube link</p>}
+                <p> {nameError && "Minimun 6 characters"}</p>
+                <p> {urlError && "Use a validate embed youtube link"}</p>
                 <input type="text" onBlur={validateLabel} className={labelsError && "error-border"} ref={labelText} placeholder="labels"></input>
                 <i className="plus-btn fas fa-plus" onClick={addLabel}></i>
                 {labels.length > 0 ? 
@@ -119,10 +143,10 @@ function FormAddVideo() {
                     </div>
                 )}
                 <input type="text" onBlur={validatePicture} className={`picture-input ${pictureError && "error-border"}`} ref={pictureText} placeholder="picture"></input>
-                {pictureError && <p className='error-label-left-middle'>Please verify the picture link. <br/>Must begin with "https://" and be .jpg, .jpeg or .bmp</p>}
+                <p className='error-label-center'> {pictureError && "Link must begin with 'https://' and be .jpg, .jpeg or .bmp"}</p>
                 <textarea type="text" onBlur={validateDescription} className={`description-input ${descriptionError && "error-border"}` } ref={descriptionText} placeholder="description"></textarea>
-                {descriptionError && <p className='error-label-bottom'>Please verify the description. Can't be less than 30 characters</p>}
-                <button type='submit' onClick={validateFields}> Submit </button>
+                <p className='error-label-center'> {descriptionError && "Description can't be less than 30 characters"}</p>
+                <button type='submit' disabled={allowSubmit ? false : true} onClick={validateFields}> Submit </button>
             </form>
         </div>
     </div>
@@ -130,3 +154,37 @@ function FormAddVideo() {
 }
 
 export default FormAddVideo
+
+
+
+/*return (
+    <div className='form-main'>
+     <video src='/videos/video-form.mp4'  type="video/mp4"  autoPlay loop muted />
+        <div className='form-add-container'>
+            <form className='form-add'>
+                <input type="text" onBlur={validateName} className={nameError && "error-border"} ref={nameText} placeholder="name"/>
+                {nameError && <p className='error-label-left-top'>Minimun 6 characters</p>}
+                <input type="text" onBlur={validateUrl} className={urlError && "error-border"} ref={urlText} placeholder="url"></input>
+                {urlError && <p className='error-label-right-top'>Use a validate embed youtube link</p>}
+                <input type="text" onBlur={validateLabel} className={labelsError && "error-border"} ref={labelText} placeholder="labels"></input>
+                <i className="plus-btn fas fa-plus" onClick={addLabel}></i>
+                {labels.length > 0 ? 
+                (
+                    <div className='labels-container'>
+                        {labels.map((label)=> (<p> <i className="plus-btn fas fa-minus-circle" onClick={()=>deleteLabel(label)}></i> {label}</p>))}
+                    </div>
+                ):
+                (
+                    <div className='labels-container'>
+                        <p>There are no labels added</p>
+                    </div>
+                )}
+                <input type="text" onBlur={validatePicture} className={`picture-input ${pictureError && "error-border"}`} ref={pictureText} placeholder="picture"></input>
+                {pictureError && <p className='error-label-left-middle'>Link must begin with "https://" and be .jpg, .jpeg or .bmp</p>}
+                <textarea type="text" onBlur={validateDescription} className={`description-input ${descriptionError && "error-border"}` } ref={descriptionText} placeholder="description"></textarea>
+                {descriptionError && <p className='error-label-bottom'>Description can't be less than 30 characters</p>}
+                <button type='submit' onClick={validateFields}> Submit </button>
+            </form>
+        </div>
+    </div>
+  )*/
