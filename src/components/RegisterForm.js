252bd4e7385c70aa687aff5fffe252109afbf707
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-12-17 20:31:22 -0300

    register form OK

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -3,14 +3,14 @@ import { useDispatch } from '../store/StoreProvider';
 import "./RegisterForm.css"
 
 import  {firebaseApp}  from "../credentials";
-import {getAuth, signOut} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, signOut} from 'firebase/auth'
 import {getFirestore,collection,addDoc,getDocs,doc,deleteDoc,getDoc,setDoc} from "firebase/firestore"
 
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp); //ADD A BASE DE DATOS
 
 function RegisterForm() {
-    const dispatch = useDispatch();
+    const [register, setRegister] = useState(false)
     const [passwordOneVisible, setPasswordOneVisible] = useState(false)
     const [passwordTwoVisible, setPasswordTwoVisible] = useState(false)
     const [nameError, setNameError] = useState(false);
@@ -19,37 +19,42 @@ function RegisterForm() {
     const [dateError, setDateError] = useState(false);
     const [passwordErrorOne, setPasswordErrorOne] = useState(false);
     const [passwordErrorTwo, setPasswordErrorTwo] = useState(false);
-    const [passwordEqualError, setPasswordEqualError] = useState(false)
-    const [allowSubmit, setAllowSubmit] = useState(false)
+    const [passwordEqualError, setPasswordEqualError] = useState(false);
+    const [allowSubmit, setAllowSubmit] = useState(false);
     const emailText = useRef();
     const nameText = useRef();
     const lastnameText = useRef();
     const dateText = useRef();
     const passwordOneText = useRef();
     const passwordTwoText = useRef();
-
-  const addVideo = ()=>{
-    dispatch(
-      {
-        type: "ADD",
-        payload:{
-          name:nameText.current.value, 
-          url:lastnameText.current.value,
-          //theme: labels,
-         // picture: pictureText.current.value,
-         // description: descriptionText.current.value
-        }
-      }
-    )
-  }
+    const genreFemBtn = useRef();
 
   const toogleVisibility = (e)=>{
-    console.log(e.currentTarget)
+    if(e.current.type==="text")
+    {
+        e.current.type = "password"
+        if(e.current.id === "passwordOne")
+        {
+            setPasswordOneVisible(true)
+        }else{
+            setPasswordTwoVisible(true)
+        }
+    }else
+    {
+        e.current.type="text"
+        if(e.current.id === "passwordOne")
+        {
+            setPasswordOneVisible(false)
+        }else{
+            setPasswordTwoVisible(false)
+        }
+    }
+  
   }
 
     const validateName = ()=>{
         enableButton();
-        if(!/\D/.test(nameText.current.value) && nameText.current.value.length > 5)
+        if(/\D/.test(nameText.current.value) || nameText.current.value.length < 6)
         {
             setNameError(false)
         }else{
@@ -59,16 +64,12 @@ function RegisterForm() {
 
     const validateLastname = ()=>{
         enableButton();
-        if(!/\D/.test(nameText.current.value) && nameText.current.value.length > 5)
+        if(/\D/.test(nameText.current.value)|| nameText.current.value.length <6)
         {
             setLastnameError(false)
         }else{
             setLastnameError(true)
         }          
-    }
-    const validateUrl = ()=>{
-        enableButton();
-        setLastnameError(!lastnameText.current.value.includes("www.youtube.com/embed"))
     }
    
     const validateEmail = ()=>{
@@ -106,35 +107,37 @@ function RegisterForm() {
             }
     }
     
-    const updateList = ()=>{
-        
-        if(!nameError&&!lastnameError&&!emailError&&!dateError&&!passwordErrorOne)
+    const submitRequest = async (e)=>{
+        e.preventDefault()
+        let genre = genreFemBtn.current.checked ? "Female" : "Male";
+        let idLogin =  await createUserWithEmailAndPassword(auth,emailText.current.value,passwordOneText.current.value);
+        if(!nameError&&!lastnameError&&!emailError&&!dateError&&!passwordErrorOne&&!passwordErrorTwo)
         {
-            //addVideo();   
-            /*try{  
-                alert("Estoy agregando")
-                await addDoc(collection(db,'videos'),{
+            console.log(idLogin)
+            try{  
+                await addDoc(collection(db,'users'),{
+                    userId: idLogin.user.uid,
                     name:nameText.current.value, 
-                    url:lastnameText.current.value,
-                    theme: labels,
-                    picture: pictureText.current.value,
-                    description: descriptionText.current.value,
-                    views:0
+                    lastname:lastnameText.current.value,
+                    email: emailText.current.value,
+                    dateOfBirth: dateText.current.value,
+                    genre: genre,
+                    role: "admin"
                 })
             }catch(err){
                 console.log(err)
-            }*/
+            }
         }
     }
 
     const validateFields = (e)=>{
         e.preventDefault();
         validateName();
-        validateUrl();
+        validateLastname();
         validateEmail();
         validateDate();
         validateEmail();
-        updateList();
+        submitRequest();
     }
     
   return (
@@ -146,14 +149,14 @@ function RegisterForm() {
                 <input type="text" onBlur={validateLastname} className={lastnameError && "error-border"} ref={lastnameText} placeholder="lastname"></input>
                 <p> {nameError && "Minimun 6 characters. Must contain at least one letter"}</p>
                 <p> {lastnameError && "Minimun 6 characters. Must contain at least one letter"}</p>   
-                <input type="email" onBlur={validateEmail} className={`email-input ${emailError && "error-border"}` } 
+                <input type="email" onBlur={validateEmail} className={`email-input ${emailError && "error-border"}`} 
                 ref={emailText} placeholder="email"></input>
                 <p className='error-label-center'> {emailError && "Use a valid email address"}</p>     
                 <input type="date" ref={dateText} onBlur={validateDate} className={dateError && "error-border"} ></input>   
                 <div className='radio-btn-container'>
                     <div className='female-radiobtn'>
                         <span class="radio-checkbox__text">Female</span>
-                        <input class="radio-checkbox__input" type="radio" name="genre" value="female" checked/>
+                        <input class="radio-checkbox__input" ref={genreFemBtn} type="radio" name="genre" value="female" checked/>
                     </div>
                     <div className='male-radiobtn'>
                         <span class="radio-checkbox__text">Male</span>
@@ -162,7 +165,7 @@ function RegisterForm() {
                 </div>
                 <p className='error-label-center'> {dateError && "Date of birth can't be larger than today"}</p>
                 <div className='password-container'>
-                    <input type="password" onBlur={validatePassword} className={`${(passwordErrorOne || passwordEqualError) && "error-border"}` } ref={passwordOneText} placeholder="Password"></input>
+                    <input type="password" id='passwordOne' onBlur={validatePassword} className={`${(passwordErrorOne || passwordEqualError) && "error-border"}` } ref={passwordOneText} placeholder="Password"></input>
                     <i onClick={()=>toogleVisibility(passwordOneText)} class={`${passwordOneVisible ? "fas fa-eye" : "fas fa-eye-slash" }`}></i>
                 </div>
                 <div className='password-container'>
@@ -171,7 +174,7 @@ function RegisterForm() {
                 </div>
                 <p> {passwordErrorOne && "Password can't be less than 8 characters"}<br/>{passwordEqualError && "Passwords do not match"}</p>
                 <p> {passwordErrorTwo && "Password can't be less than 8 characters"}</p>
-                <button type='submit' disabled={allowSubmit ? false : true} onClick={validateFields}> Submit </button>
+                <button type='submit' disabled={allowSubmit ? false : true} onClick={submitRequest}> Submit </button>
             </form>
         </div>
     </div>
