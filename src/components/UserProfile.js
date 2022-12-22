3663d84368571861709130718dbcb9eb8ee2917a
Ignacio0104 <ignacio.smirlian@hotmail.com>
2022-12-22 11:17:28 -0300

    update photo OK

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,20 +4,19 @@ import { useNavigate } from 'react-router-dom'
 import "./UserProfile.css"
 import  {firebaseApp}  from "../credentials";
 import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
-import {getFirestore,collection,addDoc, query, getDocs, where, doc, updateDoc} from "firebase/firestore"
+import {getFirestore, doc, updateDoc} from "firebase/firestore"
 
 const db = getFirestore(firebaseApp); //ADD A BASE DE DATOS
 
 function UserProfile(props) {
   const [editMode, setEditMode] = useState(false);
-  const [allowSubmit, setAllowSubmit] = useState(false)
-  const [error, setError] = useState(false)
+  const [allowSubmit, setAllowSubmit] = useState(false);
+  const [gender, setGender] = useState("")
+  const [error, setError] = useState(false);
   const [nameError, setNameError] = useState(false);
   const [lastnameError, setLastnameError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
   const [dateError, setDateError] = useState(false);
   const dateTextRef = useRef();
-  const emailTextRef = useRef();
   const nameTextRef = useRef();
   const lastnameTextRef = useRef();
 
@@ -62,12 +61,17 @@ const validateDate = ()=>{
     setAllowSubmit(true);
 }
 
+    const updateGender = (gender)=>{
+      setGender(gender);
+      localStorage.setItem("genre",gender);
+    }
+
     const updateRegister = async (e)=>{
       e.preventDefault();
       validateDate();
       validateName();
       validateLastname();
-      if(!nameError&&!lastnameError&&!emailError&&!dateError)
+      if(!nameError&&!lastnameError&&!dateError)
       {
         const userReference = doc(db, "users", user.docId);
         localStorage.setItem("userName",nameTextRef.current.value);
@@ -76,7 +80,8 @@ const validateDate = ()=>{
         await updateDoc(userReference, {
           name: nameTextRef.current.value,
           lastname: lastnameTextRef.current.value,
-          dateOfBirth: dateTextRef.current.value
+          dateOfBirth: dateTextRef.current.value,
+          genre: localStorage.getItem("genre")
         });
         setError(false)
         toogleEditMode();
@@ -85,8 +90,12 @@ const validateDate = ()=>{
       }    
     }
     const toogleEditMode = ()=>{
-      setAllowSubmit(false)
+      setAllowSubmit(false);
       setEditMode(!editMode);
+      if(!editMode)
+      {
+        setError(false)
+      }
     }
 
     const logout = ()=>{     
@@ -100,13 +109,20 @@ const validateDate = ()=>{
       <div className='profile-container'>
         <div className='profile-pic-container'>
           <img width="300px" height="300px" src={user.genre==="Female" ? "/images/icon-women-profile.png" :"/images/icon-men-profile.png"} alt="profile"></img>
+          { editMode && (
+            <div>
+            <p> <span onClick={()=>updateGender("Male")} style={{fontWeight: gender==="Male" && "bold"}} className='select-maleFemale'>Male</span> | 
+            <span onClick={()=>updateGender("Female")} style={{fontWeight: gender==="Female" && "bold"}} className='select-maleFemale'>Female</span></p>
+            </div>
+            )
+            }
           <p onClick={logout}>Logout <i class="fas fa-sign-out-alt"></i></p>
         </div>
         <div className='profile-info-container'>
           <div className='profile-info-title'>
-            <h2>{editMode ? <input onBlur={validateName} ref={nameTextRef} placeholder="Name" type="text"></input> : user.name + " "} 
-            {editMode ? <input onInput={validateLastname} ref={lastnameTextRef} placeholder="Lastname" type="text"></input> : user.lastname} </h2>
-            <i onClick={toogleEditMode} class="fas fa-edit"></i>    
+            <h2>{editMode ? <input onBlur={validateName} ref={nameTextRef} placeholder="Name"  type="text"></input> : user.name + " "} 
+            {editMode ? <input onInput={validateLastname} ref={lastnameTextRef} placeholder="Lastname"  type="text"></input> : user.lastname} </h2>
+            <i onClick={toogleEditMode} style={{cursor:"pointer"}} class="fas fa-edit"></i>    
           </div> 
           <h4> Date of Birth: {editMode ? <input onBlur={validateDate} ref={dateTextRef} type="date"></input> :  user.dateOfBirth}</h4>
           <h4> Email: {user.email}</h4>
